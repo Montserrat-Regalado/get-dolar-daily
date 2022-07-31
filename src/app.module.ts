dec2f28fc146e6f95a01fa1d526b7edb80a000e8
Montserrat Regalado <montserratregalado24@gmail.com>
2022-07-31 17:35:26 -0500

    Adding nest js config and env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { GetCurrencyDataService } from './api/get-currency-data/get-currency-data.service';
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env'
+    }),
+  ],
   controllers: [],
   providers: [GetCurrencyDataService],
 })
-export class AppModule {}
+export class AppModule { }
