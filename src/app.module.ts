1a4bea44e1c7a1d5abff63ea54f1a1e1adc8c995
Montserrat Regalado <montserratregalado24@gmail.com>
2022-07-31 18:29:43 -0500

    creating services to get currency data

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { GetCurrencyDataService } from './api/get-currency-data/get-currency-data.service';
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env'
+      envFilePath: '.env',
     }),
+    HttpModule,
   ],
   controllers: [],
   providers: [GetCurrencyDataService],
 })
-export class AppModule { }
+export class AppModule {}
